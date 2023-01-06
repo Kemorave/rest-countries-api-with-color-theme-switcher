3cbec0f509b7697a9d8f9a0985c59fed5e3e095b
Kemorave <HomaDirar@Hotmail.com>
2023-01-07 01:56:21 +0200

    uo

--- src/features/country/country-card.tsx
+++ src/features/country/country-card.tsx
@@ -7,7 +7,7 @@ const CountryCard = (props: { country: Country }) => {
      <div onClick={()=>{
       navigation(`/${props.country.name.official}/`,{});
      }} className="element m-5 transition-transform   scale-100 hover:scale-105
-     cursor-pointer overflow-clip w-[18em] md:mx-7 md:w-[13em] rounded-md ">
+     cursor-pointer overflow-clip w-[18em] md:mx-7 md:w-[15em] rounded-md ">
       <img alt="country" className="h-[10rem] w-full object-cover" src={props.country.flags.svg} />
      <div className="p-5 flex flex-col gap-1">
      <p className="font-bold mb-2">{props.country.name.official}</p>
